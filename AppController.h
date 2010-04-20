08c47f3c6fea97cc44bb325c516162b8a7a713bc
beepscore <support@beepscore.com>
2010-04-20 12:56:53 -0700

    Add AppController class.

---
+++ AppController.h
@@ -0,0 +1,16 @@
+//
+//  AppController.h
+//  ThreadDraw
+//
+//  Created by Steve Baker on 4/20/10.
+//  Copyright 2010 Beepscore LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AppController : NSObject {
+
+}
+
+@end
---
+++ AppController.m
@@ -0,0 +1,14 @@
+//
+//  AppController.m
+//  ThreadDraw
+//
+//  Created by Steve Baker on 4/20/10.
+//  Copyright 2010 Beepscore LLC. All rights reserved.
+//
+
+#import "AppController.h"
+
+
+@implementation AppController
+
+@end
